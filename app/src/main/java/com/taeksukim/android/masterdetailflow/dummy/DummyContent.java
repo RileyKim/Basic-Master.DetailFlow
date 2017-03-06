f843bafd2333040890a1cf329082edd4f8533d83
TaeksuKim <taeka89@gmail.com>
2017-03-06 20:05:58 +0900

    Basic Master/Detail Flow

---
+++ app/src/main/java/com/taeksukim/android/masterdetailflow/dummy/DummyContent.java
@@ -0,0 +1,61 @@
+package com.taeksukim.android.masterdetailflow.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DummyContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+
+    static{
+        addItem(new DummyItem("1", "ebookFrenzy", "http://www.ebookfrenzy"));
+        addItem(new DummyItem("2", "Amazon", "http://www.amazon.com"));
+        addItem(new DummyItem("3", "New York Times", "http://www.nytimes.com"));
+
+    }
+
+
+
+    private static void addItem(DummyItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+
+
+    /**
+     * A dummy item representing a piece of content.
+     */
+    public static class DummyItem {
+        public final String id;
+        public final String website_name;
+        public final String website_url;
+
+        public DummyItem(String id, String website_name, String website_url) {
+            this.id = id;
+            this.website_name = website_name;
+            this.website_url = website_url;
+        }
+
+        @Override
+        public String toString() {
+            return website_name;
+        }
+    }
+}
